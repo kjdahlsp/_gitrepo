975d710235c9de32a208e0b050671346f999aa65
Kalle <kalle.dahl@gmail.com>
2018-06-13 18:36:02 +0200

    Checked in solution

---
+++ NoSpoilers/NoSpoilers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NoSpoilers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
